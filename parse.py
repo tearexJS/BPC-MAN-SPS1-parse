4c2cc637472d8a84a3b9860d25740b16f199f81a
tearexJS <olinovak96@gmail.com>
2021-10-16 00:47:15 +0200

    hugh mungus changes

--- parse.py
+++ parse.py
@@ -1,4 +1,39 @@
+#made by InspektorKiddo_VUC :)
+
+import pandas as pd
+import re
+
 def main():
+    
+    data = pd.read_excel('Zdroj.xlsx', skiprows=7)
+    data = data.apply(str)
+    data = str(data)
+    data = data.replace("            ", ",").replace(", ",",").replace(",-.",",0.").replace(",-,",",0,")
+    data = data[:-43]
+    #print(data)
+    pattern = r"([^\d.,]+)[,. ]([^\d,]+)[,]([^\d,.]+)[,.](\d)[,](\d)"
+    matches = re.findall(pattern, data)
+
+    df = {
+        'Name':[],
+        'University':[],
+        'Country':[],
+        'Number of publications':[],
+        'Number of citations':[]
+
+    }
+
+    for match in matches:
+        df['Name'].append(match[0])
+        df['University'].append(match[1])
+        df['Country'].append(match[2])
+        df['Number of publications'].append(match[3])
+        df['Number of citations'].append(match[4])
+
+    df = pd.DataFrame(df,columns=['Name', 'University', 'Number of publications', 'Number of citations'])
+    writer = pd.ExcelWriter("output.xlsx")
+    df.to_excel(writer)
+    writer.save()
 
 if __name__ == '__main__':
     main()
